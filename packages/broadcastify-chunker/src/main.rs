9a58734298042bcf7eb61a2bd8e3d66e9f614110
Ritik Mishra <ritik.mishra314@gmail.com>
2023-04-22 15:03:32 -0700

    🎉 broadcastify to PCM over UDP works!

--- packages/broadcastify-chunker/src/main.rs
+++ packages/broadcastify-chunker/src/main.rs
@@ -1,42 +1,84 @@
+use gst::{prelude::*, Bin, DebugGraphDetails, Pad};
 use gstreamer as gst;
-use gst::prelude::*;
+
 
 fn main() {
     // Initialize GStreamer
     gst::init().unwrap();
 
-    // Build the pipeline
-    let uri =
-        "https://broadcastify.cdnstream1.com/26569";
-    let pipeline = gst::parse_launch(&format!("playbin uri={uri}")).unwrap();
+    let uri = "https://broadcastify.cdnstream1.com/26569";
 
-    // Start playing
-    pipeline
-        .set_state(gst::State::Playing)
-        .expect("Unable to set the pipeline to the `Playing` state");
+    uwu(uri);
 
-    // Wait until error or EOS
+    // let pipeline = gst::parse_launch(&format!("playbin uri={uri}")).unwrap();
+}
+
+fn uwu(uri: impl Into<String>) {
+    // Initialize GStreamer
+    gst::init().unwrap();
+
+    //// Build pipeline
+    let pipeline = gst::Pipeline::new(None);
+
+    // Get the broadcastify stream from the internet
+    let filesrc = gst::ElementFactory::make("playbin")
+        .name("broadcastify_reader")
+        .property("uri", uri.into())
+        .build()
+        .unwrap();
+    pipeline.add_many(&[&filesrc]).unwrap();
+
+    // By default, `playbin` will attempt to use `alsasink` as its sink. 
+    // This is undesired -- we want to send PCM over UDP, so we create a bin to acheive this.
+    let sinkbin = gst::Bin::new(Some("my fave sink bin"));
+    filesrc.set_property("audio-sink", &sinkbin);
+
+    // TODO: remove because the audio is already raw
+    let rawaudioparse = gst::ElementFactory::make("rawaudioparse")
+        .name("RAWWW AUDIOO PARSE")
+        .build()
+        .unwrap();
+    let udp_sink = gst::ElementFactory::make("udpsink")
+        .property("host", "127.0.0.1")
+        .property("port", 5555)
+        .build()
+        .unwrap();
+    sinkbin.add_many(&[&rawaudioparse, &udp_sink]).unwrap();
+
+    let sinkbin_sinkpad =
+        gst::GhostPad::with_target(Some("sink"), &rawaudioparse.static_pad("sink").unwrap())
+            .unwrap();
+    sinkbin.add_pad(&sinkbin_sinkpad).unwrap();
+    rawaudioparse.link(&udp_sink).unwrap();
+    
+    // Start pipeline
+    pipeline.set_state(gst::State::Playing).unwrap();
+
+    println!("playing :)");
+
+    // Wait for pipeline to finish
     let bus = pipeline.bus().unwrap();
     for msg in bus.iter_timed(gst::ClockTime::NONE) {
-        use gst::MessageView;
-
         match msg.view() {
-            MessageView::Eos(..) => break,
-            MessageView::Error(err) => {
-                println!(
-                    "Error from {:?}: {} ({:?})",
-                    err.src().map(|s| s.path_string()),
-                    err.error(),
-                    err.debug()
+            gst::MessageView::Eos(..) => break,
+            gst::MessageView::Error(err) => {
+                eprintln!(
+                    "Error from {:?}: {}",
+                    err.src().map(|e| e.path_string()),
+                    err.error()
                 );
                 break;
             }
-            _ => (),
+            _ => {
+                gst::debug_bin_to_dot_file(
+                    pipeline.upcast_ref::<Bin>(),
+                    DebugGraphDetails::ALL,
+                    "uwu.dot",
+                );
+            }
         }
     }
 
-    // Shutdown pipeline
-    pipeline
-        .set_state(gst::State::Null)
-        .expect("Unable to set the pipeline to the `Null` state");
+    // Stop pipeline and clean up
+    pipeline.set_state(gst::State::Null).unwrap();
 }
