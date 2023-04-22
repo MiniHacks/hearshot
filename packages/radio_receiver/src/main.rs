c89ffa7a9f0ab3fe4d5ff23993fe25aa9d8bae31
Ritik Mishra <ritik.mishra314@gmail.com>
2023-04-22 09:45:29 -0700

    socket2

--- packages/radio_receiver/src/main.rs
+++ packages/radio_receiver/src/main.rs
@@ -1,9 +1,10 @@
 use std::io::Write;
 use std::net::{Ipv4Addr, UdpSocket};
+use std::sync::mpsc::channel;
 
 fn main() {
     let mut raw_samples_socket = (|| {
-        let mut sock = UdpSocket::bind("0.0.0.0:5678")?;
+        let mut sock = UdpSocket::bind("10.42.0.1:5678")?;
         sock.set_broadcast(true)?;
         sock.connect((Ipv4Addr::BROADCAST, 5678))?;
         Ok::<_, std::io::Error>(sock)
@@ -14,43 +15,94 @@ fn main() {
 
     // let mut file = std::fs::File::create("out.raw").unwrap();
 
+    let mut gains = [0_i32; 32];
+    println!(
+        "Existing settings! 
+        - Center  freq: {:?}
+        - sample rate: {:?}
+        - ppm: {:?}
+        - tuner gains: {:?}",
+        ctl.center_freq(),
+        ctl.sample_rate(),
+        ctl.ppm(),
+        ctl.tuner_gains(&mut gains),
+    );
+
+    let (byte_tx, byte_rx) = channel();
+
     ctl.enable_agc().unwrap();
     ctl.set_ppm(0).unwrap();
     // Frequency
     ctl.set_center_freq(92_143_000).unwrap();
     // How wide of a bandwidth are we doing?
-    ctl.set_bandwidth(200_000).unwrap();
+    ctl.set_bandwidth(100_000).unwrap();
+    ctl.set_sample_rate(230_400).unwrap();
 
-    reader
-        .read_async(4, 32768, |bytes| {
-            let mut iq_floats = bytes
-                .chunks(2)
-                .map(|iq| {
-                    let to_float = |x| ((x as f32) / 127.5_f32) - 1.0;
-                    let (i, q) = (iq[0], iq[1]);
-                    demodulation::IQSample::new(to_float(i), to_float(q))
-                })
-                .collect::<Vec<_>>();
+    println!(
+        "New settings! 
+        - Center  freq: {:?}
+        - sample rate: {:?}
+        - ppm: {:?}
+        - tuner gains: {:?}",
+        ctl.center_freq(),
+        ctl.sample_rate(),
+        ctl.ppm(),
+        ctl.tuner_gains(&mut gains),
+    );
 
-            let audio_floats = demodulation::demodulate(
-                &mut iq_floats,
-                demodulation::FMRadioConfig {
-                    bandwidth: 100_000,
-                    samplerate: 230_400,
-                    deviation: 75_000,
-                },
-            );
+    let t1 = std::thread::spawn(move || loop {
+        let mut x = Vec::new();
 
+        for _ in 0..100 {
+            let audio_floats: Vec<f32> = byte_rx.recv().unwrap();
+            x.push(audio_floats);
+        }
+
+        for audio_floats in x {
             let send_buf = {
                 let num_bytes = audio_floats.len() * core::mem::size_of::<f32>();
                 let data_ptr = audio_floats.as_ptr().cast::<u8>();
                 unsafe { core::slice::from_raw_parts(data_ptr, num_bytes) }
             };
-            if let Some(e) = raw_samples_socket.send(send_buf).err() {
+            let part_len = send_buf.len() / 2;
+            if let Some(e) = raw_samples_socket
+                .send(&send_buf[0..part_len])
+                .and(raw_samples_socket.send(&send_buf[part_len..]))
+                .err()
+            {
                 eprintln!("send error: {e:?}");
             }
-        })
-        .unwrap();
+        }
+    });
+
+    let t2 = std::thread::spawn(move || {
+        reader
+            .read_async(32, 32768, |bytes| {
+                let mut iq_floats = bytes
+                    .chunks(2)
+                    .map(|iq| {
+                        let to_float = |x| ((x as f32) / 127.5_f32) - 1.0;
+                        let (i, q) = (iq[0], iq[1]);
+                        demodulation::IQSample::new(to_float(i), to_float(q))
+                    })
+                    .collect::<Vec<_>>();
+
+                let audio_floats = demodulation::demodulate(
+                    &mut iq_floats,
+                    demodulation::FMRadioConfig {
+                        bandwidth: 100_000,
+                        samplerate: 230_400,
+                        deviation: 75_000,
+                    },
+                );
+
+                byte_tx.send(audio_floats).unwrap();
+            })
+            .unwrap()
+    });
+
+    t1.join();
+    t2.join();
 }
 
 mod demodulation {
@@ -100,8 +152,9 @@ mod demodulation {
         // Actually do the demodulation + resampling!
         /////
         let raw_audio = fm_demod.demodulate_block(samples);
-        let raw_audio = babycat::Waveform::new(samplerate, 1, raw_audio);
-        let resampled_audio = raw_audio.resample(DESIRED_SAMPLE_RATE).unwrap();
-        resampled_audio.into()
+        raw_audio
+        // let raw_audio = babycat::Waveform::new(samplerate, 1, raw_audio);
+        // let resampled_audio = raw_audio.resample(DESIRED_SAMPLE_RATE).unwrap();
+        // resampled_audio.into()
     }
 }
