1390e008a13346911cdf442778b50079a2ea1153
mini <gh.mini.zou@gmail.com>
2023-04-22 14:30:07 -0700

    🍞 Stop following the Figma

--- packages/mobile/components/Breadcrumb.js
+++ packages/mobile/components/Breadcrumb.js
@@ -2,10 +2,13 @@ import React from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function Navigator({ navigation, pageName }) {
+export default function Breadcrumb({ navigation, pageName }) {
   return (
     <Pressable
-      style={{ flexDirection: "row" }}
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+      }}
       onPress={() => navigation.pop()}
     >
       <MaterialCommunityIcons name="chevron-left" color="#C7C7C7" size={28} />
@@ -17,6 +20,7 @@ export default function Navigator({ navigation, pageName }) {
 const styles = StyleSheet.create({
   title: {
     color: "#C7C7C7",
-    fontSize: 24,
+    fontSize: 28,
+    fontWeight: "bold",
   },
 });
